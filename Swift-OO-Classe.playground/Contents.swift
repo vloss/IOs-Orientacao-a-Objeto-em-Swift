d21feb71079d5d82f44d30a8f00a5592cf8d2045
vloss <viniciusvianaloss@gmail.com>
2023-01-06 18:43:34 -0300

    Iniciando Classes de OO com Swift.

---
+++ Swift-OO-Classe.playground/Contents.swift
@@ -0,0 +1,14 @@
+// Classes
+
+class person {
+    
+    var name: String
+    var gender: Character
+    var age: Int
+    
+    // Constrututor
+    init(name: String, gender: Character) {
+        self.name = name
+        self.gender = gender
+    }
+}
