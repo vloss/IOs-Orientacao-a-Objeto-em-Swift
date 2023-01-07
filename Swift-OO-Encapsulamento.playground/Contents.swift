d6608771099b139abf71c599db3af6872bc0df3c
vloss <viniciusvianaloss@gmail.com>
2023-01-07 00:42:48 -0300

    Continuação de encapsulamento.

--- Swift-OO-Encapsulamento.playground/Contents.swift
+++ Swift-OO-Encapsulamento.playground/Contents.swift
@@ -1,3 +1,27 @@
-import UIKit
+// Encapsulamento
 
-var greeting = "Hello, playground"
+class CreditCard{
+    let number: String
+    let limit: Double
+    let invoiceValue: Double = 0
+    
+    let iof: Double = 6.38
+    
+    init(number: String, limit: Double) {
+        self.number = number
+        self.limit  = limit
+    }
+    
+    func makePurchase (off value: Double, usingCreditCard: Bool) -> Bool {
+        
+        let debit = usingCreditCard ? value * (1 + iof/100) : value
+        let finalValue = invoiceValue + debit
+        
+        if finalValue > limit{
+            return false
+        } else {
+            invoiceValue = finalValue
+            return true
+        }
+    }
+}
