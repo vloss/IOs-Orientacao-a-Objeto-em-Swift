d6960e3037fe0fdb4595524b95996e815af2d5a4
vloss <viniciusvianaloss@gmail.com>
2023-01-08 12:37:59 -0300

    Finalizando encapsulamento em swift.

--- Swift-OO-Encapsulamento.playground/Contents.swift
+++ Swift-OO-Encapsulamento.playground/Contents.swift
@@ -1,9 +1,12 @@
 // Encapsulamento
 
 class CreditCard{
+
     let number: String
     let limit: Double
-    let invoiceValue: Double = 0
+     
+    //Quando privado só a classe acessa.
+    private var invoiceValue: Double = 0
     
     let iof: Double = 6.38
     
@@ -12,9 +15,10 @@ class CreditCard{
         self.limit  = limit
     }
     
-    func makePurchase (off value: Double, usingCreditCard: Bool) -> Bool {
+    // Adicionando o final o metodo não pode ser modificado por uma classe filha.
+    final func makePurchase (off value: Double, isInternational: Bool) -> Bool {
         
-        let debit = usingCreditCard ? value * (1 + iof/100) : value
+        let debit = isInternational ? value * (1 + iof/100) : value
         let finalValue = invoiceValue + debit
         
         if finalValue > limit{
@@ -24,4 +28,48 @@ class CreditCard{
             return true
         }
     }
+    
+    func getInvoice() -> Double {
+        return invoiceValue
+    }
+    
+    var invoice: Double {
+        return invoiceValue
+    }
 }
+
+
+let creditCard = CreditCard(number: "1245-9876-2893-1837", limit: 5000)
+
+creditCard.makePurchase(off: 1500, isInternational: true)
+
+creditCard.limit
+
+// Quando não é privado o invoiceValue pode se acessar direto
+//creditCard.invoiceValue
+//creditCard.invoiceValue = 0
+
+creditCard.invoice
+creditCard.getInvoice()
+
+
+// Criando cartão de crédito e débito que herda classe CreditCard
+class DebitCreditCard: CreditCard {
+
+    private var _balance: Double = 0
+    
+    var balance: Double {
+        return _balance
+    }
+    
+    func makeTransaction(of value: Double){
+        _balance += value
+    }
+}
+
+let debitCard = DebitCreditCard(number: "9876-9876-2893-1837", limit: 5000)
+
+debitCard.makeTransaction(of: 500)
+debitCard.makeTransaction(of: -100)
+
+print(debitCard.balance)
