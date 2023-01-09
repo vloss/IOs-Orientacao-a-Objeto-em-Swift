ef458b9fd42959deba336cd84c2191167627ef8a
vloss <viniciusvianaloss@gmail.com>
2023-01-09 16:42:15 -0300

    Finalizando Extensions.

--- Swift-OO-Extensions.playground/Contents.swift
+++ Swift-OO-Extensions.playground/Contents.swift
@@ -1,3 +1,53 @@
-import UIKit
+// Extensions
 
-var greeting = "Hello, playground"
+import Foundation
+
+let name = "Vinicius Viana Loss"
+
+extension String {
+    
+//    var vowels: [Character] {
+//    var list: [Character] = []
+//
+//        for character in characters {
+//
+//            switch String(character).lowercased() {
+//            case "a", "e", "i", "o", "u":
+//                list.append(character)
+//            default:
+//                break
+//            }
+//        }
+//
+//        return list
+//    }
+    
+    func initials() -> String {
+        let arrya = self.components(separatedBy: " ")
+        let firsts = arrya.map({$0.first!})
+        
+        return String(firsts)
+    }
+}
+
+name.initials()
+
+
+// Utilizando Extensions para Classes
+class Account {
+    let number: String
+    var balance: Double = 0
+    
+    init(number: String) {
+        self.number = number
+    }
+}
+
+extension Account {
+    convenience init(number: String, balance: Double) {
+        self.number = number
+        self.balance = balance
+    }
+}
+
+let account = Account(number: "986966355423452")
